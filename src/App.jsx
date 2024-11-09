b5a3016c4396426b951dadfadc7b058b171db83e
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-11-10 01:59:43 +0530

    fix: signin redirect to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import LandingPage from "@/pages/LandingPage";
 import LogIn from "@/pages/LogIn";
-// import ContactPage from "@/routes/contact";
-// import InvoicesPage from "@/routes/Dash.invoices";
-// import DashboardPage from "@/routes/Dash";
-// import SignInPage from "@/routes/Sign-in";
-// import SignUpPage from "@/routes/Sign-up";
 import AnalyticsDash from "@/pages/IndividualAnalytics";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<LogIn />} />
-        {/* <Route path="/contact" element={<ContactPage />} /> */}
         <Route path="/analytics" element={<AnalyticsDash />} />
       </Routes>
-    </div>
+    </Router>
   );
 };
 
