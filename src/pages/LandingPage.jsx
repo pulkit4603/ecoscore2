aa0cde680361635f93f302794df170958f23afd4
unfortunatelygeek <yilingmatriarch@proton.me>
2024-11-09 22:42:10 +0530

    merge update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,43 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { Navigation } from "../components/landing/navigation";
-import { Header } from "../components/landing/header";
-import { Features } from "../components/landing/features";
-import { About } from "../components/landing/about";
-import { Services } from "../components/landing/services";
-import { Gallery } from "../components/landing/gallery";
-import { Testimonials } from "../components/landing/testimonials";
-import { Team } from "../components/landing/Team";
-import { Contact } from "../components/landing/contact";
-import JsonData from "../data/data.json";
-import SmoothScroll from "smooth-scroll";
-import "./landing.css";
+  import React, { useState, useEffect } from "react";
+  import { Navigation } from "../components/landing/navigation";
+  import { Header } from "../components/landing/header";
+  import { Features } from "../components/landing/features";
+  import { About } from "../components/landing/about";
+  import { Services } from "../components/landing/services";
+  import { Gallery } from "../components/landing/gallery";
+  import { Testimonials } from "../components/landing/testimonials";
+  import { Team } from "../components/landing/Team";
+  import { Contact } from "../components/landing/contact";
+  import JsonData from "../data/data.json";
+  import SmoothScroll from "smooth-scroll";
+  import "./landing.css";
 
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
+  export const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true,
+  });
 
-const LandingPage = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+  const LandingPage = () => {
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+      setLandingPageData(JsonData);
+    }, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
-};
+    return (
+      <div>
+        <Navigation />
+        <Header data={landingPageData.Header} />
+        <Features data={landingPageData.Features} />
+        <About data={landingPageData.About} />
+        <Services data={landingPageData.Services} />
+        <Gallery data={landingPageData.Gallery} />
+        <Testimonials data={landingPageData.Testimonials} />
+        <Team data={landingPageData.Team} />
+        <Contact data={landingPageData.Contact} />
+      </div>
+    );
+  };
 
-export default function LandingPage() {
-  return <div> LandingPage </div>;
-}
+  export default LandingPage;
