905aa8a97aebdaaf1bade0f5c228146ddb02189c
Aditi <Aditi@THE-HILL>
2024-11-10 00:28:11 +0530

    page: user analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import LandingPage from "@/pages/LandingPage";
 import LogIn from "@/pages/LogIn";
+import ContactPage from "@/routes/Contact";
+// import InvoicesPage from "@/routes/Dash.invoices";
+// import DashboardPage from "@/routes/Dash";
+// import SignInPage from "@/routes/Sign-in";
+// import SignUpPage from "@/routes/Sign-up";
+import AnalyticsDash from "@/pages/IndividualAnalytics";
 
 const App = () => {
   return (
@@ -11,6 +17,8 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/analytics" element={<AnalyticsDash />} />
       </Routes>
     </div>
   );
