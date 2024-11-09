66588de11e462df0153f22511d5c5caf61966b95
Pulkit Dwivedi <pulkitdwivedi123@gmail.com>
2024-11-10 05:25:45 +0530

    repush

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { userName } from "@/data/data.jsx";
 import PdSidebar from "@/components/pd-dashboard/PdSidebar";
 import { Score } from "@/components/pd-dashboard/Score";
 import { ScoreHistory } from "@/components/pd-dashboard/ScoreHistory";
+import { useUser } from "@clerk/clerk-react";
 import ActivitiesCard from "@/components/pd-dashboard/ActivitiesCard";
 import Chatbot from "./Chatbot";
 import AppliancesCard from "@/components/pd-dashboard/AppliancesCard";
@@ -31,13 +32,15 @@ export default function Dashboard() {
   );
 }
 
-// Dummy dashboard component with content
 const UserWelcome = () => {
+  const { user, isLoaded } = useUser();
   return (
     <div className="flex w-full">
       <div className="p-2 md:p-10 border-none bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <Card className="bg-gradient-to-r p-6 from-green-500 to-green-700 text-white shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl">
-          <CardTitle className="pb-6">Hello {userName}</CardTitle>
+          <CardTitle className="pb-6">
+            Hello {isLoaded ? user.fullName : "User"}
+          </CardTitle>
           <CardContent className="p-0">
             Welcome to EcoScore: Your Personal Energy Manager
           </CardContent>
